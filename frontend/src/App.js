7802aef2f8c669a869bab0f20cc9d697e7f6fd79
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-01-26 11:19:15 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import "./App.css";
-import Employee from "./Components/Employee/Employee";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import DepartmentForm from "./components/Department/DepartmentForm";
+import DepartmentList from "./components/Department/DepartmentList";
+import EmployeeForm from "./components/Employee/EmployeeForm";
+import EmployeeList from "./components/Employee/EmployeeList";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Employee />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/departments" exact component={DepartmentList} />
+        <Route path="/departments/add" component={DepartmentForm} />
+        <Route path="/employees" exact component={EmployeeList} />
+        <Route path="/employees/add" component={EmployeeForm} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
